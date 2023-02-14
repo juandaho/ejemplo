440868180bfd79c00a2e63930bb1a1e264825c2a
Juan David Holguin <juandaho@gmail.com>
2023-02-14 12:38:40 -0500

    creación del index

---
+++ ejemplo1.js
@@ -0,0 +1,23 @@
+class Persona 
+{
+
+    constructor(nombre, apellido)
+    {
+
+        this.nombre= nombre;
+        this.apellido= apellido; 
+
+    }
+
+}
+
+
+
+
+
+let persona1 = new Persona ("Juan", "Perez");
+
+persona1.nombre="Pedros";
+
+console.log(persona1);
+console.log(persona1.nombre);
